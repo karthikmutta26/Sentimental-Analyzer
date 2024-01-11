8146958986c3dab8fd81e07f086b012055fd3adf
karthikmutta26 <112752579+karthikmutta26@users.noreply.github.com>
2024-01-11 12:07:48 +0530

    app.py

--- main/app.py
+++ main/app.py
@@ -36,46 +36,37 @@ def analyze():
 
     if selected_analyzer == 'flipkart':
         url_base = request.form['url']
-        sentiment_counts, plot_url,wordcloud_url = analyze_flipkart(url_base)
+        sentiment_counts, plot_url, wordcloud_url = analyze_flipkart(url_base)
     elif selected_analyzer == 'snapdeal':
         url_base = request.form['url']
-        sentiment_counts, plot_url,wordcloud_url = analyze_snapdeal(url_base)
+        sentiment_counts, plot_url, wordcloud_url = analyze_snapdeal(url_base)
     elif selected_analyzer == 'nykaa':
         url_base = request.form['url']
-        sentiment_counts, plot_url,wordcloud_url = analyze_nykaa(url_base)
+        sentiment_counts, plot_url, wordcloud_url = analyze_nykaa(url_base)
     else:
         return "Invalid analyzer selected."
 
-    return render_template('result.html', sentiment_counts=sentiment_counts, plot_url=plot_url,wordcloud_url=wordcloud_url)
+    return render_template('result.html', sentiment_counts=sentiment_counts, plot_url=plot_url, wordcloud_url=wordcloud_url)
 
 
-
-
-def sentiment_Vader(text):
-
+def sentiment_Vader(text, sid):
     if text.isdigit():
         rating = int(text)
-
-        # Define your own criteria for categorizing ratings into positive, neutral, and negative
         if rating >= 4:
             return "positive"
         elif rating >= 2:
             return "neutral"
         else:
             return "negative"
-            
-    # If the input is not numeric, perform sentiment analysis using VADER
     else:
         over_all_polarity = sid.polarity_scores(text)
         print(f"Sentiment analysis result: {over_all_polarity}")
-
         if over_all_polarity['compound'] >= 0.05:
             return "positive"
         elif over_all_polarity['compound'] <= -0.05:
             return "negative"
         else:
             return "neutral"
-
 
 
 def analyze_flipkart(url_base):
@@ -92,56 +83,16 @@ def analyze_flipkart(url_base):
         soup = BeautifulSoup(response.content, 'html.parser')
 
         review_divs = soup.find_all('div', {'class': 't-ZTKy'})
-
         reviews = [c.div.div.get_text(strip=True) for c in review_divs]
         all_reviews.extend(reviews)
 
     # Save all reviews to CSV
-    
 
     # Perform sentiment analysis on the collected reviews
     data = pd.DataFrame({'review': all_reviews})
-    data['polarity'] = data['review'].apply(lambda review: sentiment_Vader(review))
+    data['polarity'] = data['review'].apply(lambda review: sentiment_Vader(review, flipkart_sid))
 
-
-
-    # Display the count of each sentiment category
-    sentiment_counts = data['polarity'].value_counts()
-
-    # Ensure all sentiments are included in the pie chart
-    all_sentiments = ['positive', 'neutral', 'negative']
-    sentiment_counts = sentiment_counts.reindex(all_sentiments, fill_value=0)
-
-    # Create a pie chart
-    custom_colors = ['#21de74', '#3053cf', '#f00f1f']  # Green, Blue, Red
-    plt.figure(figsize=(10, 10))
-    plt.pie(sentiment_counts, labels=sentiment_counts.index, autopct='%1.1f%%', startangle=140, colors=custom_colors)
-    plt.title('Sentiment Analysis Results')
-
-    # Save the plot to a BytesIO object
-    img = BytesIO()
-    plt.savefig(img, format='png')
-    img.seek(0)
-
-    # Encode the plot to base64 for displaying in HTML
-    plot_url = base64.b64encode(img.getvalue()).decode()
-
-    all_reviews_text = ' '.join(all_reviews)
-    wordcloud = WordCloud(width=500, height=300, background_color='white').generate(all_reviews_text)
-
-    plt.figure(figsize=(10, 10))
-    plt.imshow(wordcloud, interpolation='bilinear')
-    plt.axis('off')
-
-    # Save the word cloud to a BytesIO object
-    wordcloud_img = BytesIO()
-    plt.savefig(wordcloud_img, format='png')
-    wordcloud_img.seek(0)
-
-    # Encode the word cloud to base64 for displaying in HTML
-    wordcloud_url = base64.b64encode(wordcloud_img.getvalue()).decode()
-
-    return sentiment_counts, plot_url,wordcloud_url
+    return generate_results(data)
 
 
 def analyze_snapdeal(url_base):
@@ -158,62 +109,18 @@ def analyze_snapdeal(url_base):
         soup = BeautifulSoup(response.content, 'html.parser')
 
         review_divs = soup.find_all('div', {'class': 'user-review'})
-        reviews = []  # Clear the list for each page
-
-        for j in range(len(review_divs)):
-            reviews.append(review_divs[j].find("p").text)
-
+        reviews = [review_div.find("p").text for review_div in review_divs]
         all_reviews.extend(reviews)
 
     all_reviews = list(set(all_reviews))
 
-
     # Save all reviews to CSV
-    
 
     # Perform sentiment analysis on the collected reviews
     data = pd.DataFrame({'review': all_reviews})
-    data['polarity'] = data['review'].apply(lambda review: sentiment_Vader(review))
+    data['polarity'] = data['review'].apply(lambda review: sentiment_Vader(review, snapdeal_sid))
 
-
-
-    # Display the count of each sentiment category
-    sentiment_counts = data['polarity'].value_counts()
-
-    # Ensure all sentiments are included in the pie chart
-    all_sentiments = ['positive', 'neutral', 'negative']
-    sentiment_counts = sentiment_counts.reindex(all_sentiments, fill_value=0)
-
-    # Create a pie chart
-    custom_colors = ['#21de74', '#3053cf', '#f00f1f']  # Green, Blue, Red
-    plt.figure(figsize=(10, 10))
-    plt.pie(sentiment_counts, labels=sentiment_counts.index, autopct='%1.1f%%', startangle=140, colors=custom_colors)
-    plt.title('Sentiment Analysis Results')
-
-    # Save the plot to a BytesIO object
-    img = BytesIO()
-    plt.savefig(img, format='png')
-    img.seek(0)
-
-    # Encode the plot to base64 for displaying in HTML
-    plot_url = base64.b64encode(img.getvalue()).decode()
-
-    all_reviews_text = ' '.join(all_reviews)
-    wordcloud = WordCloud(width=500, height=300, background_color='white').generate(all_reviews_text)
-
-    plt.figure(figsize=(10, 10))
-    plt.imshow(wordcloud, interpolation='bilinear')
-    plt.axis('off')
-
-    # Save the word cloud to a BytesIO object
-    wordcloud_img = BytesIO()
-    plt.savefig(wordcloud_img, format='png')
-    wordcloud_img.seek(0)
-
-    # Encode the word cloud to base64 for displaying in HTML
-    wordcloud_url = base64.b64encode(wordcloud_img.getvalue()).decode()
-
-    return sentiment_counts, plot_url,wordcloud_url
+    return generate_results(data)
 
 
 def analyze_nykaa(url_base):
@@ -230,25 +137,21 @@ def analyze_nykaa(url_base):
         soup = BeautifulSoup(response.content, 'html.parser')
 
         review_divs = soup.find_all('div', {'class': 'css-ggpltb'})
-        reviews = []  # Clear the list for each page
-
-        for j in range(len(review_divs)):
-            reviews.append(review_divs[j].find("h4").text)
-
+        reviews = [review_div.find("h4").text for review_div in review_divs]
         all_reviews.extend(reviews)
 
     all_reviews = list(set(all_reviews))
 
-
     # Save all reviews to CSV
-    
 
     # Perform sentiment analysis on the collected reviews
     data = pd.DataFrame({'review': all_reviews})
-    data['polarity'] = data['review'].apply(lambda review: sentiment_Vader(review))
+    data['polarity'] = data['review'].apply(lambda review: sentiment_Vader(review, nykaa_sid))
+
+    return generate_results(data)
 
 
-
+def generate_results(data):
     # Display the count of each sentiment category
     sentiment_counts = data['polarity'].value_counts()
 
@@ -270,7 +173,7 @@ def analyze_nykaa(url_base):
     # Encode the plot to base64 for displaying in HTML
     plot_url = base64.b64encode(img.getvalue()).decode()
 
-    all_reviews_text = ' '.join(all_reviews)
+    all_reviews_text = ' '.join(data['review'])
     wordcloud = WordCloud(width=500, height=300, background_color='white').generate(all_reviews_text)
 
     plt.figure(figsize=(10, 10))
@@ -285,9 +188,8 @@ def analyze_nykaa(url_base):
     # Encode the word cloud to base64 for displaying in HTML
     wordcloud_url = base64.b64encode(wordcloud_img.getvalue()).decode()
 
-    return sentiment_counts, plot_url,wordcloud_url
+    return sentiment_counts, plot_url, wordcloud_url
 
 
 if __name__ == '__main__':
-    sid = SentimentIntensityAnalyzer()
     app.run(debug=True)
